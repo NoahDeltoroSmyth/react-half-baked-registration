520c3574ec2901b9a757ea7a57898004f880b839
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-01-10 17:21:38 -0800

    logout button

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import { useState } from 'react';
 import './App.css';
-import { getUser } from './services/users';
-import SignIn from './views/SignIn';
-import SignUp from './views/SignUp';
+import { getUser, logout } from './services/users';
+// import SignIn from './views/SignIn';
+// import SignUp from './views/SignUp';
+import Auth from './views/Auth';
 
 function App() {
   const [currentUser, setCurrentUser] = useState(getUser());
+
+  const logoutUser = async () => {
+    await logout();
+    setCurrentUser(null);
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/signup">
+          {/* <Route exact path="/signup">
             {currentUser && <h1>Signed in</h1>}
             {!currentUser && <SignUp setCurrentUser={setCurrentUser} />}
           </Route>
           <Route exact path="/signin">
             {currentUser && <h1>Signed in</h1>}
             {!currentUser && <SignIn setCurrentUser={setCurrentUser} />}
-          </Route>
+          </Route> */}
           <Route exact path="/">
-            {currentUser && <h1>Signed in</h1>}
-            {!currentUser && <SignUp setCurrentUser={setCurrentUser} />}
+            {currentUser && (
+              <>
+                <h1>User is signed in</h1>
+                <button onClick={logoutUser}>Logout</button>
+              </>
+            )}
+            {!currentUser && <Auth setCurrentUser={setCurrentUser} />}
           </Route>
         </Switch>
       </BrowserRouter>
--- src/views/Auth.js
+++ src/views/Auth.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { signInUser, signUpUser } from '../services/users';
 import AuthForm from '../components/AuthForm';
+import classNames from 'classnames';
+import './Auth.css';
 
 export default function Auth({ setCurrentUser }) {
   const [type, setType] = useState('signin');
@@ -19,19 +21,39 @@ export default function Auth({ setCurrentUser }) {
       }
       setCurrentUser(resp);
     } catch {
-      setError('Error');
+      setError('No user found with that email/password');
     }
   };
   return (
-    <div>
-      <AuthForm
-        error={error}
-        email={email}
-        setEmail={setEmail}
-        password={password}
-        setPassword={setPassword}
-        handleSubmit={handleSubmit}
-      />
-    </div>
+    <>
+      <div className="link">
+        <h1
+          onClick={() => {
+            setType('signin');
+          }}
+          className={classNames({ active: type === 'signin' })}
+        >
+          Sign In
+        </h1>
+        <h1
+          onClick={() => {
+            setType('signup');
+          }}
+          className={classNames({ active: type === 'signup' })}
+        >
+          Sign Up
+        </h1>
+      </div>
+      <div>
+        <AuthForm
+          error={error}
+          email={email}
+          setEmail={setEmail}
+          password={password}
+          setPassword={setPassword}
+          handleSubmit={handleSubmit}
+        />
+      </div>
+    </>
   );
 }
